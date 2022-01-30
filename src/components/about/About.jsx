64685a82a84a9c6b0556c1c4ed83085722453810
riyajawa <riya1119.cse19@chitkara.edu.in>
2022-01-30 21:47:41 +0530

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,29 @@
+import "./about.scss";
+
+
+export default function About(){
+    return(
+        <div className="about" id="about">
+        <div className="left">
+        <div className="wrapper">
+            <h2>Hi There, I'm</h2>
+            <h1>Riya</h1>
+            
+          </div>
+          <div className="imgContainer">
+            
+          </div>
+        </div>
+        <div className="right">
+          <div className="wrapper">
+            <h2>Hi There, I'm</h2>
+            <h1>Riya</h1>
+            
+          </div>
+          {/* <a href="#portfolio">
+            <img src="assets/down.png" alt="" />
+          </a> */}
+        </div>
+      </div>
+    );
+}
